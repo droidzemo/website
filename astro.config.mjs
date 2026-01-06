8d24240f3bd8fa249e855652bdebcd3f89b344ba
k <k@kian>
2026-01-06 23:56:56 +0100

    Configure deployment for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://droidzemo.github.io',
+    base: '/website',
     integrations: [
         starlight({
             title: 'Nigamelastic',
